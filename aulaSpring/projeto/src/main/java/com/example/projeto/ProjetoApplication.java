9320ea6dee596405965fb75a5f122b677b297e8f
tiagosmcp <103256731+tiagosmcp@users.noreply.github.com>
2025-10-03 12:14:21 +0000

    Revert ".."
    This reverts commit 9bb92b5ef6bf747946d0c81d006457990f1925b4.

--- aulaSpring/projeto/src/main/java/com/example/projeto/ProjetoApplication.java
+++ aulaSpring/projeto/src/main/java/com/example/projeto/ProjetoApplication.java
@@ -9,4 +9,6 @@ public class ProjetoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
 	}
+
 }
+
--- aulaSpring/projeto/src/main/java/com/example/projeto/model/Pessoa.java
+++ aulaSpring/projeto/src/main/java/com/example/projeto/model/Pessoa.java
@@ -1,46 +1,49 @@
 package com.example.projeto.model;
 
+import javax.annotation.processing.Generated;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedType;
 
 @Entity
 @Table(name = "pessoas")
 public class Pessoa{
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
     private Integer idade;
 
-    public Pessoa() {}
+    public Pessoa(){
+        this("",0);
+    }
 
     public Pessoa(String nome, Integer idade){
         this.nome = nome;
         this.idade = idade;
     }
 
-    public Long getId() {
-        return id;
+    public String getNome(){
+        return this.nome;
+    };
+    public void setNome(String nome){
+        this.nome= nome;
+    };
+
+
+    public Integer getIdade(){
+        return this.idade;
+    };
+    public void setIdade(Integer idade){
+        this.idade= idade;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
 }
+
+
+   
